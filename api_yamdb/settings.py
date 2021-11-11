d635a9b139e36cf0466582f00bc3955c2f04c0c4
solilov <alexandr.solilov@yandex.ru>
2021-11-11 18:44:34 +0300

    After2 review

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -135,7 +135,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = ['/var/www/static/', ]
+STATICFILES_DIRS = [
+    '/var/www/static/',
+]
 
 
 MEDIA_URL = "/media/"
